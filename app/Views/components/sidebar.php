3d427bb7a99d6d57802c8feb51cc234ddb850f04
SinggihNaufal <singgihnaufal03@gmail.com>
2025-06-24 19:41:30 +0700

    menambahkan api data transaksi pembelian

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -20,24 +20,35 @@
                     <span>Keranjang</span>
                 </a>
             </li><!-- End Keranjang Nav -->
+
             <li class="nav-item">
                 <a class="nav-link <?php echo (uri_string() == 'produk') ? "" : "collapsed" ?>" href="produk">
                     <i class="bi bi-receipt"></i>
                     <span>Produk</span>
                 </a>
             </li><!-- End Produk Nav -->
+
             <li class="nav-item">
                 <a class="nav-link <?php echo (uri_string() == 'produk kategori') ? "" : "collapsed" ?>" href="produk_kategori">
                     <i class="bi bi-receipt"></i>
                     <span>Produk Kategori</span>
                 </a>
             </li><!-- End Produk Kategori Nav -->
+
+            <li class="nav-item">
+                <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+                    <i class="bi bi-person"></i>
+                    <span>Profile</span>
+                </a>
+            </li><!-- End Profile Nav -->
+
             <li class="nav-item">
                 <a class="nav-link <?php echo (uri_string() == 'contact') ? "" : "collapsed" ?>" href="contact">
                     <i class="bi bi-receipt"></i>
                     <span>Contact</span>
                 </a>
             </li><!-- End Contact Nav -->
+
         <?php
         }
         ?>
